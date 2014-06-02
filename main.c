c4eeb33ddd9b85086cefaf60b7bb9c951183690f
wojcikk2903 <wojcikk2903@gmail.com>
2014-06-02 23:45:44 +0200

    działające tworzenie pierwszej macierzy pomocniczej do iteracji

--- main.c
+++ main.c
@@ -33,15 +33,12 @@ void forward_subst(matrix *m, double *x, double *b)
 {
     int el_ind = 0;
     int col = 0;
-    // x[m->n-1] = b[0]/m->vals[el_ind];
     x[0] = b[0]/m->vals[el_ind];
 
     for (int i = 1; i < m->n; i++)
     {
-        // el_ind = i < m->n-1 ? m->row_start_ind[i+1]-1 : m->nelements-1;
         el_ind = m->row_start_ind[i];
-        // col = m->col_ind[el_ind];
-         col = m->col_ind[el_ind];
+        col = m->col_ind[el_ind];
         double accumulated = 0.0;
         while (col < i)
         {
@@ -51,6 +48,42 @@ void forward_subst(matrix *m, double *x, double *b)
         }
         x[i] = (b[i]-accumulated)/m->vals[el_ind];
     }
+}
+
+int find_diag_el_ind(matrix *a, int row)
+{
+    int el_ind = a->row_start_ind[row];
+    while (a->col_ind[el_ind] < row)
+        el_ind++;
+
+    return el_ind;
+}
+
+matrix get_iterative_matrix1(matrix *a, double w)
+{
+    matrix it_matrix;
+    matrix am = *a;
+    it_matrix.vals = malloc(am.nelements*sizeof(double));
+    it_matrix.col_ind = malloc(am.nelements*sizeof(int));
+    it_matrix.row_start_ind = malloc(am.n*sizeof(int));
+
+    int it_el_ind = 0;
+    for (int i = 0; i < am.n; i++)
+    {
+        int diag_el_ind = find_diag_el_ind(a, i);
+        double diag_el_value = am.vals[diag_el_ind];
+        
+        it_matrix.row_start_ind[i] = it_el_ind;
+        for (int j = am.row_start_ind[i]; j < diag_el_ind; j++)
+        {
+            it_matrix.vals[it_el_ind] = -w*am.vals[j]/diag_el_value;
+            it_matrix.col_ind[it_el_ind] = am.col_ind[j];
+            it_el_ind++;
+        }
+        it_matrix.vals[it_el_ind++] = 1-2*w;
+    }
+
+    return it_matrix;
 }
 
 int double_equals (double a, double b)
@@ -133,6 +166,37 @@ void test()
     if (!double_equals(-0.5, vector3[2]))
         all_passed = FALSE;
     
+    if (!all_passed)
+        printf("Coś nie tak\n");
+    else
+        printf("\n----------------------------\n\tWszystko w porządku\n----------------------------\n");
+
+    printf("\n--------------------------------\n\tSzukanie pierwszej macierzy do iteracji\n-----------------------\n");
+    double vals4[] = { 3.0, 1.0, 2.0, 1.0, 5.0, 8.0};
+    int col_ind4[] = {0, 1, 2, 0, 1, 2};
+    int row_start_ind4[] = {0, 1, 3};
+    m.vals = vals4;
+    m.n = 3;
+    m.nelements = 6;
+    m.col_ind = col_ind4;
+    m.row_start_ind = row_start_ind4;
+    matrix it_matrix1 = get_iterative_matrix1(&m, 1.5);
+    printf("m1.vals[0] = %.2f\n", it_matrix1.vals[0]);
+    printf("m1.vals[1] = %.2f\n", it_matrix1.vals[1]);
+    printf("m1.vals[2] = %.2f\n", it_matrix1.vals[2]);
+    printf("m1.vals[3] = %.2f\n", it_matrix1.vals[3]);
+    printf("m1.vals[4] = %.2f\n", it_matrix1.vals[4]);
+    all_passed = TRUE;
+    if (!double_equals(-2.0, it_matrix1.vals[0]))
+        all_passed = FALSE;
+    if (!double_equals(-2.0, it_matrix1.vals[1]))
+        all_passed = FALSE;
+    if (!double_equals(-0.1875, it_matrix1.vals[2]))
+        all_passed = FALSE;
+    if (!double_equals(-0.9375, it_matrix1.vals[3]))
+        all_passed = FALSE;
+    if (!double_equals(-2.0, it_matrix1.vals[4]))
+        all_passed = FALSE;
     if (!all_passed)
         printf("Coś nie tak\n");
     else
